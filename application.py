4f68400e41c803610db745635e53efdbfc6a1b9d
Ben Dichter <ben.dichter@gmail.com>
2017-03-25 19:01:27 -0700

    app: render on same page and add tense option

--- application.py
+++ application.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
 from change_tense.change_tense import change_tense
-import pdb
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    return render_template('form.html')
+def start():
+    return render_template('form.html', text_in='enter text here', text_out='', tense='past')
+
 
 @app.route('/result/', methods=['GET', 'POST'])
 def result():
-    text = request.form['input_text']
-    text_out = change_tense(text, 'future')
-    return render_template('result.html', input_text=text, text=text_out)
+    if 'input_text' in request.form:
+        text_in = request.form['input_text']
+    else:
+        text_in = ''
+    text_out = change_tense(text_in, request.form['tense'])
+    return render_template('form.html', text_in=text_in, text_out=text_out, tense=request.form['tense'])
