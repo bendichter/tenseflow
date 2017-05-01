a652e5b66398f8c7622a4c71f4fd419b799fba96
Ben Dichter <ben.dichter@gmail.com>
2017-05-01 09:18:12 -0700

    database stores corrections

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from tenseflow.database import db_session
+from tenseflow.models import Answer
+
 
 from tenseflow import change_tense
 
@@ -18,14 +20,32 @@ def start():
 
 @app.route('/result/', methods=['GET', 'POST'])
 def result():
+    print(list(request.form.iteritems()))
     if 'input_text' in request.form:
         text_in = request.form['input_text']
+        tense = request.form['tense']
     else:
         text_in = ''
+        tense = ''
+
     try:
         text_out = change_tense(text_in, request.form['tense'])
     except:
         text_out = 'ERROR!!!!!!'
+
+    if 'correction' in request.form:
+        correction = request.form['correction']
+    else:
+        correction = ''
+
+    if 'errortick' in request.form:
+        incorrect = request.form['errortick']
+    else:
+        incorrect = False
+
+    db_session.add(Answer(text_in, tense, text_out, incorrect=incorrect, correction=correction))
+    db_session.commit()
+
     return render_template('form.html', text_in=text_in, text_out=text_out, tense=request.form['tense'])
 
 if __name__ == "__main__":
--- tenseflow/models.py
+++ tenseflow/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Boolean
 from tenseflow.database import Base
 
 MAX_LEN = 1000
@@ -6,15 +6,18 @@ MAX_LEN = 1000
 
 class Answer(Base):
     __tablename__ = 'answers'
-    text_in = Column(String(MAX_LEN), primary_key=True)
-    text_out = Column(String(MAX_LEN), unique=True)
-    correction = Column(String(MAX_LEN), unique=True)
+    id = Column(Integer, primary_key=True)
+    text_in = Column(String(MAX_LEN))
+    text_out = Column(String(MAX_LEN))
+    correction = Column(String(MAX_LEN))
+    incorrect = Column(Boolean)
 
-    def __init__(self, text_in=None, tense=None, text_out=None, correction=None):
+    def __init__(self, text_in=None, tense=None, text_out=None, incorrect=False, correction=None):
         self.text_in = text_in
         self.tense = tense
         self.text_out = text_out
         self.correction = correction
+        self.incorrect = incorrect
 
     def __repr__(self):
-        return self.text_in
+        return str(self.id) + ': ' + self.text_in[:10]
